b020f63bbd89d907e6b0056c1ef6308c5e16aa8d
Jérôme DUBOS <jeromedubos67@gmail.com>
2025-03-08 19:11:43 +0100

    Bouton caché admin dans le footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import LegalModal from "../LegalModal/LegalModal";
 import "./Footer.css";
 import { FaGithub, FaInstagram } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleAdminAccess = () => {
+    navigate('/admin');
+  };
+
   return (
     <footer className="footer">
       <div className="footer-container">
         <p className="footer-text">
           &copy; {new Date().getFullYear()} Dubos Web Services. Tous droits réservés.
+          <span className="admin-access" onClick={handleAdminAccess}></span>
         </p>
         <nav className="footer-nav">
           <a href="https://github.com/Jerome-Dubos" target="_blank" rel="noopener noreferrer" className="footer-icon">
